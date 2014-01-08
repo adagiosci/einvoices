918207c56c0eefe320512c7dcd61c3d8ff4946c2
Irving Sulub <isulub@zaviatravel.com>
2014-01-08 10:42:23 -0600

    fix

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+find  ./ -iname "*~" > backup.txt
+while read line
+do
+	echo -e "$line\n"
+	rm "$line"
+done < backup.txt
+rm backup.txt
+
+
+find  ./ -iname "*~" > backup.txt
+while read line
+do
+	echo -e "$line\n"
+        rm "$line"
+done < backup.txt
+rm backup.txt
+
+#git push
+
