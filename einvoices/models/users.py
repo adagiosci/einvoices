c23847dae1e4e12b87f421ac59bf6f81aec0837f
lmartinez <lmartinez@Madagascar>
2014-01-11 21:06:13 -0600

    model de usuarios

---
+++ einvoices/models/users.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+    String,
+    )
+
+from .meta import (
+	Base,
+	DBSession,
+)
+
+class Company(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(45))
+    password = Column(String(45))
+    first_name = Column(String(100))
+    second_name = Column(String(100))
+    last_name = Column(String(100))
+    mother_name = Column(String(100))
