8ea49438d2152c5ba52d0b67f37b8634dec5d9d3
lmartinez <lmartinez@Madagascar>
2014-01-12 10:42:27 -0600

    view de login 0.0

---
+++ einvoices/views/login.py
@@ -0,0 +1,22 @@
+from pyramid.renderers import get_renderer
+from pyramid.response import Response
+from pyramid.decorator import reify
+from pyramid.view import view_config
+from layouts import Layouts
+BASE_TMPL = 'einvoices:templates/'
+
+from sqlalchemy.exc import DBAPIError
+
+from einvoices.models.users import (
+    DBSession,
+    Users,
+    )
+
+class ProjectorLogin(Layouts):
+	def __init__(self, request):
+		self.request = request
+	
+	@view_config(renderer=BASE_TMPL + "login/login.pt", name='login')
+	def login_view (self):
+		users = DBSession.query(Users)
+		return {"users": users.all()}
