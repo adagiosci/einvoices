01aebddd506ab66a956891b24002293e409df28d
Irving Sulub <isulub@zaviatravel.com>
2014-01-27 21:49:32 -0600

    fix

--- suppliers.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE suppliers (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,rfc VARCHAR(45) NOT NULL, contacto VARCHAR(45), noExterior VARCHAR(20), colonia VARCHAR(45), localidad VARCHAR(45), pais ENUM('Mexico'), cp INT UNSIGNED, razonsocial VARCHAR(45), calle VARCHAR(45), noInterior VARCHAR(20), referencia VARCHAR(45) NOT NULL, municipio VARCHAR(25), estado VARCHAR(25), email VARCHAR(45));
-
-
-CREATE TABLE suppliers (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,rfc VARCHAR(45) NOT NULL, contacto VARCHAR(45), noExterior VARCHAR(20), colonia VARCHAR(45), localidad VARCHAR(45), pais VARCHAR(45), cp INT UNSIGNED, razonsocial VARCHAR(45), calle VARCHAR(45), noInterior VARCHAR(20), referencia VARCHAR(45) NOT NULL, municipio VARCHAR(25), estado VARCHAR(25), email VARCHAR(45));
