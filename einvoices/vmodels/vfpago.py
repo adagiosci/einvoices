1134aa22806b2c156a3938a9cb09d1d76f43ffdf
lmartinez <lmartinez@Madagascar>
2014-02-26 11:26:30 -0600

    nuevos modelos

---
+++ einvoices/vmodels/vfpago.py
@@ -0,0 +1,29 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+    String,
+    ForeignKey,
+    )
+
+from .meta import (
+	Base,
+	DBSession,
+)
+from sqlalchemy.orm import relationship, backref
+
+class FPago(Base):
+    __tablename__ = 'FPago'
+    id = Column(Integer, primary_key=True)
+    Clave = Column(String(50))
+    Descripcion = Column(String(50))
+    Tipo = Column(String(50))
+    Moneda = Column(String(50))
+    id_Empresa = Column(Integer,ForeignKey('companies.id'))
+    tenant_id = Column(String(5))
+    
+    #relationships
+    company = relationship("Company", foreign_keys=[idcompany], backref=backref('clients', order_by=id))
+    
+
