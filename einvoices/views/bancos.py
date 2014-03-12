9881f1c6dcab69cc695ee306cb3a564cd227f2fa
Irving Sulub <isulub@zaviatravel.com>
2014-03-11 22:51:39 -0600

    direccion

--- einvoices/views/bancos.py
+++ einvoices/views/bancos.py
@@ -30,25 +30,25 @@ class ProjectorBancos(Main):
 		self.tSucursal = Sucursal
 		self.tCompany = Company
 		self.config_view_name = 'bancos'
-		super(ProjectorMonedas,self).__init__(request)
+		super(ProjectorBancos,self).__init__(request)
 		
 	@action(renderer=BASE_TMPL  + "bancos/index.pt")
 	def index(self):
 		msj = self.message();
-		entries = self.DBSession.query(Currency).filter_by(company_id = self.__current_company__.id)
+		entries = self.DBSession.query(Banco).filter_by(company_id = self.__current_company__.id)
 		pages = webhelpers.paginate.Page(entries, page=self.request.GET.get('p',1), items_per_page=20)
 		_pagination = self._pagination(pages)
-		return {'currencies':pages,'msj':msj,'pagination':_pagination}
+		return {'bancos':pages,'msj':msj,'pagination':_pagination}
 		
 	@action(renderer=BASE_TMPL  + "bancos/edit.pt")
 	def edit(self):
 		msj = self.message();
 		id = self.request.matchdict['id']
-		entries = self.DBSession.query(Currency).filter_by(company_id = self.__current_company__.id)
+		entries = self.DBSession.query(Banco).filter_by(company_id = self.__current_company__.id)
 		pages = webhelpers.paginate.Page(entries, page=self.request.GET.get('p',1), items_per_page=20)
 		_pagination = self._pagination(pages)
-		entry = self.DBSession.query(Currency).filter_by(id=id,company_id = self.__current_company__.id).first()
-		return {'entry':entry,'currencies':pages,'msj':msj,'pagination':_pagination}
+		entry = self.DBSession.query(Banco).filter_by(id=id,company_id = self.__current_company__.id).first()
+		return {'entry':entry,'bancos':pages,'msj':msj,'pagination':_pagination}
 	
 	@action(renderer=BASE_TMPL  + "bancos/list.pt")
 	def filter(self):
@@ -58,33 +58,37 @@ class ProjectorBancos(Main):
 		return {'companies':page,'pagination':_pagination}
 	
 	@reify
-	def companies_list(self):
+	def bancos_list(self):
 		renderer = get_renderer(BASE_TMPL  + "bancos/list.pt")
 		return renderer.implementation().macros
 		
 	@action()
 	def create(self):
-		currency = Currency(
-				 name = self.request.POST['name']
-				,currency_code = self.request.POST['currency_code']
-				,prefix = self.request.POST['prefix']
+		banco = Banco(
+				 Clave = self.request.POST['Clave']
+				,Nombre = self.request.POST['Nombre']
+				,Sucursal = self.request.POST['Sucursal']
+				,Direccion = self.request.POST['Direccion']
+				,Telefono = self.request.POST['Telefono']
 				,company_id = self.__current_company__.id
 			)
-		self.DBSession.add(currency)
+		self.DBSession.add(banco)
 		return HTTPFound(location='/bancos/m=ric')
 		
 	@action()
 	def update(self):
-		currency = self.DBSession.query(Currency).filter_by(id=self.request.POST['entry_id'],company_id = self.__current_company__.id).update({
-													 'name': self.request.POST['name']
-													,'currency_code': self.request.POST['currency_code']
-													,'prefix': self.request.POST['prefix']
+		banco = self.DBSession.query(Banco).filter_by(id=self.request.POST['entry_id'],company_id = self.__current_company__.id).update({
+													 'Clave': self.request.POST['Clave']
+													,'Nombre': self.request.POST['Nombre']
+													,'Sucursal': self.request.POST['Sucursal']
+													,'Direccion': self.request.POST['Direccion']
+													,'Telefono': self.request.POST['Telefono']
 													})
 		return HTTPFound(location='/bancos/m=rec')
 	
 	@action()
 	def delete(self):
 		id = self.request.matchdict['id'] 
-		sucursal = self.DBSession.query(Currency).filter_by(id=id,company_id = self.__current_company__.id).delete()
+		banco = self.DBSession.query(Banco).filter_by(id=id,company_id = self.__current_company__.id).delete()
 		return HTTPFound(location='/bancos/m=rdc')
 				
